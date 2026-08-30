e6704f61b104172f0e10a49189140e08b8e9da19
Carlos <carlossama359@gmail.com>
2026-08-29 22:14:41 -0600

    Fix missing WPF shapes namespace

--- src/NexoLauncher.App/MainWindow.ProductionShell.cs
+++ src/NexoLauncher.App/MainWindow.ProductionShell.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace NexoLauncher.App;
