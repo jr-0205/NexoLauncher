bea0ebb5460d3867f1cf6ae81c12a3fe35a536cd
Carlos <carlossama359@gmail.com>
2026-08-30 00:46:57 -0600

    Load NEXA brand styles

--- src/NexaLauncher.UI/src/main.tsx
+++ src/NexaLauncher.UI/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
 import "./styles/workflows.css";
+import "./styles/brand.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
