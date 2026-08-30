a9f5cfde679b8aa44ec6ca81b42d4c3b20ffca73
Carlos <carlossama359@gmail.com>
2026-08-30 01:41:43 -0600

    Load NEXA profile tool styles

--- src/NexaLauncher.UI/src/main.tsx
+++ src/NexaLauncher.UI/src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/index.css";
 import "./styles/workflows.css";
 import "./styles/brand.css";
 import "./styles/interaction.css";
+import "./styles/profile-tools.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
