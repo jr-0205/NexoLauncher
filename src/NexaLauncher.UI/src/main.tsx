737c810c82cffacc953ee08b1b281a706aebb132
Carlos <carlossama359@gmail.com>
2026-08-30 01:16:15 -0600

    Load NEXA interaction and scrolling rules

--- src/NexaLauncher.UI/src/main.tsx
+++ src/NexaLauncher.UI/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./app/App";
 import "./styles/index.css";
 import "./styles/workflows.css";
 import "./styles/brand.css";
+import "./styles/interaction.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
